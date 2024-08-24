0a0d7d891a5aea4ac1f4f4818b98f59d6d5775d0
RoyaAlmasimehr <almasimehrroya@gmail.com>
2024-08-24 13:49:43 +0330

    add numberInvoces

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,33 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
-import { lusitana ,inter } from "@/app/ui/fonts";
-import Image from 'next/image';
-export default function Page() {
-  return (
-    <div>
-      <p className={` ${inter.className} text-blue-700`}>
-        welcome to the tehran
-      </p>
-      <br />
-      <p className={`${lusitana.className} text-rose-700`}>
-        welcome to the tehran
-      </p>
-      <div>
-        <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={760}
-          className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
-        />
-        <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className='block md:hidden'
+// فایل: /app/dashboard/page.tsx
 
-          alt="of the dashboard project showing desktop version"
-        />
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue } from "@/app/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
       </div>
-    </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
   );
 }
